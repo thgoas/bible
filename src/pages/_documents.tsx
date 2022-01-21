3bf5749b4b98f0854d6c19890011d43ece34c669
Thiago A Salomão <thgas95@gmail.com>
2022-01-21 08:37:32 -0300

    add favicon.ico in head _document

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -17,6 +17,7 @@ export default class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
             crossOrigin="anonymous"
           ></script> */}
+          <link rel="hora do devocional icon" href="/public/favicon.ico" />
         </Head>
         <body>
           {/* 👇 Here's the script */}
